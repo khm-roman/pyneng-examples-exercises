7a30b74d27f020413c91ece38ce655c600def31c
username <username.user@example.com>
2021-02-09 18:08:13 +0000

    task 6 has been completed

--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -57,10 +57,25 @@ trunk_template = [
 access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
 trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
 
-for intf, vlan in access.items():
-    print("interface FastEthernet" + intf)
-    for command in access_template:
-        if command.endswith("access vlan"):
-            print(f" {command} {vlan}")
+#for intf, vlan in access.items():
+#    print("interface FastEthernet" + intf)
+#    for command in access_template:
+#        if command.endswith("access vlan"):
+#            print(f" {command} {vlan}")
+#        else:
+#            print(f" {command}")
+for intf, vlan in trunk.items():
+    print("interface FastEthernet " + intf)
+    for command in trunk_template:
+        if command.endswith("allowed vlan"):
+            if vlan[0] == 'add':
+                vlan.remove('add')
+                print(f" {command} add " + ','.join(vlan))
+            elif vlan[0] == 'only':
+                vlan.remove('only')
+                print(f" {command} " + ','.join(vlan))
+            elif vlan[0] == 'del':
+                vlan.remove('del')
+                print(f" {command} remove " + ' '.join(vlan))
         else:
             print(f" {command}")
