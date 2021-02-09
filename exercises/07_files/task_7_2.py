f95153860e8d078dfb63ace1c1fc8d82df0a4752
username <username.user@example.com>
2021-02-09 21:55:39 +0000

    task 7 has been partialy completed

--- exercises/07_files/task_7_2.py
+++ exercises/07_files/task_7_2.py
@@ -39,3 +39,9 @@ interface Ethernet0/3
 ...
 
 """
+from sys import argv
+name = argv[1]
+with open (name, 'r') as f:
+    for line in f:
+        if line[0] != '!':
+            print (line.rstrip())
