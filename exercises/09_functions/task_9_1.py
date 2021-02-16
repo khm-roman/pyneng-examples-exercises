6e45bbb98d2cf26760166a28cf2e0c8d66c4b20f
username <username.user@example.com>
2021-02-16 20:41:33 +0000

    task 9.1 has completed

--- exercises/09_functions/task_9_1.py
+++ exercises/09_functions/task_9_1.py
@@ -75,3 +75,16 @@ def generate_access_config(intf_vlan_mapping, access_template):
 
     Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
     """
+    i = []
+    for intf, vlan in intf_vlan_mapping.items():
+       i.append (f'interface {intf}')
+       for command in access_template:
+           if command.endswith('access vlan'):
+              i.append(f'{command} {vlan}')
+           else:
+              i.append(f'{command}')
+
+    return i
+
+result = generate_access_config(access_config_2, access_mode_template)
+print (result)
