abcb7a1b5ecf2f1ae5f4eaac8c275e0f75351850
username <username.user@example.com>
2021-02-19 21:47:00 +0000

    tasks 9 have been completed

--- exercises/09_functions/task_9_1a.py
+++ exercises/09_functions/task_9_1a.py
@@ -43,3 +43,30 @@ port_security_template = [
 ]
 
 access_config = {"FastEthernet0/12": 10, "FastEthernet0/14": 11, "FastEthernet0/16": 17}
+
+
+def generate_access_config(intf_vlan_mapping, access_template, psecurity=None ):
+    """
+    intf_vlan_mapping - словарь с соответствием интерфейс-VLAN такого вида:
+        {'FastEthernet0/12':10,
+         'FastEthernet0/14':11,
+         'FastEthernet0/16':17}
+    access_template - список команд для порта в режиме access
+
+    Возвращает список всех портов в режиме access с конфигурацией на основе шаблона
+    """
+    i = []
+    for intf, vlan in intf_vlan_mapping.items():
+       i.append (f'interface {intf}')
+       for command in access_template:
+           if command.endswith('access vlan'):
+              i.append(f'{command} {vlan}')
+           else:
+              i.append(f'{command}')
+       for command2 in port_security_template:
+           if psecurity:
+              i.append(f'{command2}')
+    return i
+
+#print(generate_access_config(access_config, access_mode_template))
+print(generate_access_config(access_config, access_mode_template, port_security_template))
--- exercises/09_functions/task_9_4.py
+++ exercises/09_functions/task_9_4.py
@@ -64,3 +64,22 @@ def ignore_command(command, ignore):
         if word in command:
             ignore_status = True
     return ignore_status
+
+
+def convert_config_to_dict (config_filename, **kwargs):
+    result = {}
+
+    with  open (config_filename) as f:
+
+        for line in f:
+           line = line.rstrip()
+           if line and not ignore_command(line, ignore) and line[0] != '!':
+               if not line.startswith(' '):
+                   dict_key = line.strip()
+                   result[dict_key] = []
+               elif line.startswith(' '):
+                   result[dict_key].append(line.strip())
+               else:
+                  continue
+        return result
+print (convert_config_to_dict('config_sw1.txt'))
