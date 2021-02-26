95c6ff4024487a7c4267d6752b8d7c7a1f999462
username <username.user@example.com>
2021-02-26 22:39:00 +0000

    tasks 11 have been completed

--- exercises/11_modules/task_11_1.py
+++ exercises/11_modules/task_11_1.py
@@ -43,8 +43,23 @@ def parse_cdp_neighbors(command_output):
     и с файлами и с выводом с оборудования.
     Плюс учимся работать с таким выводом.
     """
+    dct = {}
+#    l = command_output.strip().split('\n')
+
+    for line in command_output.strip().split('\n'):
+        columns = line.split()
+        if ">" in line:
+            d1 = line.split('>')[0]
+
+        if len(columns) >= 5 and columns[3].isdigit():
+           r_d, l_int, l_int_num, *other, r_int, r_int_num = columns
+           dct[(d1, l_int + l_int_num)] = (r_d, r_int + r_int_num)
+
+    return  dct
+
+
 
 
 if __name__ == "__main__":
-    with open("sh_cdp_n_sw1.txt") as f:
+    with open("sh_cdp_n_r3.txt") as f:
         print(parse_cdp_neighbors(f.read()))
--- exercises/11_modules/task_11_2a.py
+++ exercises/11_modules/task_11_2a.py
@@ -73,6 +73,9 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+from task_11_2 import create_network_map
+from draw_network_graph import draw_topology
+
 
 infiles = [
     "sh_cdp_n_sw1.txt",
@@ -80,3 +83,19 @@ infiles = [
     "sh_cdp_n_r2.txt",
     "sh_cdp_n_r3.txt",
 ]
+
+def unique_network_map(topology_dict):
+    dubs = []
+    dct = topology_dict.copy()
+    for key, value in dct.items():
+        if key in dct.values() and value not in dubs:
+            dubs.append(key)
+    for d in dubs:
+        del dct[d]
+
+
+    return dct
+
+if __name__ == "__main__":
+   draw_topology(unique_network_map(create_network_map(infiles)), output_filename="task_11_2aa_topology")
+#  print (unique_network_map(create_network_map(infiles)))
